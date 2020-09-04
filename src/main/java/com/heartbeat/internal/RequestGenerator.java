583779dbf5b56f7ad05b3e14af6b8f36c40bf793
Nguyen Hoang Vu <ecraft.eventagency@gmail.com>
2020-09-04 16:48:16 +0700

    minor

--- src/main/java/com/heartbeat/internal/RequestGenerator.java
+++ src/main/java/com/heartbeat/internal/RequestGenerator.java
@@ -9,7 +9,7 @@ public class RequestGenerator {
   public static String getExchangeRequest = "http://%s:%d/exchange?userid=%s&roleid=%s&server_id=%s&order_id=%s&item_id=%s&money=%d&gold=%d&time=%d&sign=%s";
 
   public static String genPaymentRequest(String sessionId, PaymentData.PaymentDto paymentPackage) throws Exception {
-    String ip       =  "127.0.0.1";//""18.141.216.52";
+    String ip       =  "18.141.216.52";//"127.0.0.1";//";
     int port        = 80;
     String userId   = "88989800";
     int    serverId = Integer.parseInt(sessionId)/ Constant.SYSTEM_INFO.MAX_USER_PER_NODE;
